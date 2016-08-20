c8420a4dfaa5d58667a60d0b7bbb0e9a03b325ab
vladiko <koganvladimir@yahoo.com>
2016-08-21 00:47:12 +0300

    small fix

--- app/controllers/fakeChecker.Controller.js
+++ app/controllers/fakeChecker.Controller.js
@@ -3,7 +3,7 @@ exports.render = function (req, res) {
     fakeCheckerDal.check('d', function (docs) {
         var names = [];
         docs.forEach(function (d) { names.push(d.name); });
-        res.send('Hello World Express Controller: ' + names.join(', '));
+        res.send('Hello World Express Controller ' + names.join(' '));
     });
 };
 //var fs = require("fs");
