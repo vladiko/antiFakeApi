c01d73ce46059fc7e47cd0e1897d2f05fdfe2dd9
vladiko <koganvladimir@yahoo.com>
2016-08-21 00:51:31 +0300

    fake checker controller clean

--- app/controllers/fakeChecker.Controller.ts
+++ app/controllers/fakeChecker.Controller.ts
@@ -3,23 +3,7 @@ exports.render = function (req, res) {
     fakeCheckerDal.check('d', (docs: Array<Object>) => {
         var names = [];
         docs.forEach((d: any) => { names.push(d.name); });
-
-        res.send('Hello World Express Controller ' + names.join(' '));
+        res.send('Hello World Express Controller: ' + names.join(', '));
     });
 };
-
-//var fs = require("fs");
-
-//exports.imgList = function (req, res) {
-//    var fileList = [];
-//    fs.readdir("./../mean3client/resoures/styles/", function (err, files) {
-//        if (err) {
-//            return console.error(err);
-//        }
-//        files.forEach(function (file) {
-//            fileList.push(files);
-//        });
-//        res.send(files);
-//    });
-//};
 
